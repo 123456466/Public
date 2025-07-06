bc02772432b087c2d4dfd717eba83d33bf4e93f8
123456466 <sunji7378@gmail.com>
2025-07-06 14:45:42 +0900

    feat: 스타일 개선 및 디테일 페이지에 뒤로가기 버튼 추가

--- to-do/app/[id]/page.tsx
+++ to-do/app/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getTodoItem } from "@/api/todo.api";
 import TodoItem from "@/components/todo/TodoItem";
+import Link from "next/link";
 import React from "react";
 
 interface DetailPageProps {
@@ -13,6 +14,12 @@ const DetailPage = async ({ params }: DetailPageProps) => {
   return (
     <section>
       <TodoItem todo={todoItem} />
+      <Link
+        href="/"
+        className="p-2 bg-gray-500 text-white rounded cursor-pointer mx-auto block w-20 text-center m-5"
+      >
+        돌아가기
+      </Link>
     </section>
   );
 };
--- to-do/app/page.tsx
+++ to-do/app/page.tsx
@@ -5,7 +5,6 @@ export default function HomePage() {
   return (
     <section>
       <div className="container mx-auto mt-5 ">
-        <div className="text-center text-2xl font-bold my-10">Todo List</div>
         <TodoForm />
         <TodoList />
       </div>
